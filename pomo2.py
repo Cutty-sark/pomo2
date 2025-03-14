db1e256a690652ab8c904899a3ef03b44a77184f
Cutty-sark <gmespaceprogram@gmail.com>
2025-03-14 22:40:36 +0000

    date

--- pomo2.py
+++ pomo2.py
@@ -1,2 +1,19 @@
 #2nd gen desktop pomodoro timer
+from tkinter import *
+from tkinter import ttk
+import time
 
+def create_window():
+    current_date = time.strftime("%A, %B %d, %Y")  
+    root = Tk()
+    root.title("Pomo2")
+    frame1 = ttk.Frame(root, padding = 10)
+    frame1.grid()
+    ttk.Label(frame1, text=current_date).grid(column=1, row=0)
+    root.mainloop()
+
+def main():
+    create_window()
+
+if __name__ == "__main__":
+    main()
