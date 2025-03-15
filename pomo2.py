61b7340a566fdc80a466cdac6b463038f259ef70
Cutty-sark <gmespaceprogram@gmail.com>
2025-03-15 09:37:34 +0000

    diagram

--- pomo2.py
+++ pomo2.py
@@ -4,13 +4,23 @@ from tkinter import ttk
 import time
 
 def create_window():
+    root = Tk() # initiating root window
+    root.title("Pomo2") # title of window
+    
+    #frame for date
     current_date = time.strftime("%A, %B %d, %Y")  
-    root = Tk()
-    root.title("Pomo2")
-    frame1 = ttk.Frame(root, padding = 10)
-    frame1.grid()
-    ttk.Label(frame1, text=current_date).grid(column=1, row=0)
+    framedate = ttk.Frame(root, padding = 10) # frame 1 for date
+    framedate.grid() # frame will be populated using grid config
+    ttk.Label(framedate, text=current_date).grid(column=0, row=0)
+    
+    #frame for inputs and timer display
+    frametimer = ttk.Frame(root, padding=10)
+    frametimer.grid()
+    
+    
     root.mainloop()
+
+    
 
 def main():
     create_window()
